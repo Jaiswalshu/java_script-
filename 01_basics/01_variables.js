020fc120ae26a246a50bf808e21df328b7890902
shubham jaiswal <shubhamjaiswalit@gmail.com>
2026-04-13 00:42:23 +0530

    new file _01 var..

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 12345
+let accountEmail = "shu@gmail.com"
+var accountPassword = "just@123"
+accountCity ="patna"
+let accountState
+
+// accountId = 2
+
+accountEmail = "hey@gmail.com";
+accountPassword = "0000"
+accountCity = "noida"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
+
+/*
+prfer do not use var becuse of scope  , function scope or block scope 
+
+*/
