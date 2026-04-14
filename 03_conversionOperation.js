0a1c7dfbea2cfe6f7897babe50371a4cfb0edd05
shubham jaiswal <shubhamjaiswalit@gmail.com>
2026-04-14 23:06:08 +0530

    conversion of string

---
+++ 03_conversionOperation.js
@@ -0,0 +1,26 @@
+// let score = true
+
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+
+
+//"33" => 33
+
+//"3abc"=> Nan
+
+let isLoggedIn =1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn)
+
+
+
+//1 - true
+//0 - false
+
+//"" -> false
+//"any value" => true
