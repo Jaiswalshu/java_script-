e7ab109f642e2c92f6955053bd09edc9c7dec429
shubham jaiswal <shubhamjaiswalit@gmail.com>
2026-04-14 23:34:02 +0530

    datatype summ

---
+++ 01_basics/05_datatupes-summury.js
@@ -0,0 +1,58 @@
+//primitive =>>>  - 
+    // 7 category -string, number, boolean, null , undefined , symbol, BigInt
+
+
+
+// const score: = 100
+
+
+// let userEmail = null;
+
+
+// const id = Symbol('123')
+
+// const anotherId =Symbol('123')
+
+
+
+// const bigNumber  = 4645646464;
+
+// console.log(bigNumber)
+
+
+
+
+
+//rfnce type (not premetive)
+    //array, objects , functions
+    
+    
+
+
+
+
+    // const heros =["shaktiman ", "naagraj" , "doga"];
+
+
+    // let myobj ={
+    //     name: "shubhuam jaiswal",
+    //     age:22
+    // }
+
+
+    // console.log(myobj)
+    // console.log(heros)
+    // console.log(heros[0])
+
+
+
+const myFunction = function(){
+    console.log("hello world");
+}
+
+
+myFunction();
+
+console.log(typeof myFunction)
+
+
