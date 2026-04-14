67c8dae23265f5211bef5370ba9bbdc05dd37aa4
shubham jaiswal <shubhamjaiswalit@gmail.com>
2026-04-14 22:56:37 +0530

    ne datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,6 +1,22 @@
 "use strict"; //this help us to js code work on real version
 
-alert(hello);  //we are running this on node js not in browser
+// alert(hello);  //we are running this on node js not in browser
 
 
+let name ="shu"
+let age = 18
 
+let isLoggedIn = false
+
+//number => 2 to power 53
+//bignit
+
+// string 
+
+//boolean help for true or false
+
+//null => standard value
+// undefeind 
+
+console.log(typeof age);
+console.log(typeof null);
