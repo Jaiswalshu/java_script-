4f63998aa01e40d0629110b3f053704b22e67292
shubham jaiswal <shubhamjaiswalit@gmail.com>
2026-04-23 12:56:35 +0530

    learning date and time concept

---
+++ 01_basics/07_dateinjs.js
@@ -0,0 +1,53 @@
+
+
+
+
+let myDate =new Date()
+
+
+// console.log(myDate)
+// // 2026-04-23T07:14:42.368Z
+
+// console.log(myDate.toDateString())
+// // Thu Apr 23 2026
+
+// console.log(myDate.toTimeString())
+// // 12:44:42 GMT+0530 (India Standard Time)
+
+
+// console.log(myDate.toJSON())
+// // 2026-04-23T07:14:42.368Z
+
+
+// console.log(myDate.toLocaleString())
+// // 4/23/2026, 12:44:42 PM
+
+
+
+// let myCreatedDate   = new Date(2023, 0 ,23)
+
+
+// console.log(myCreatedDate.toDateString());
+
+
+
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp)
+
+
+// console.log(Math.floor(Date.now()/1000));
+
+
+
+
+// let newDate = new Date()
+
+// console.log(newDate);
+
+// console.log(newDate.getHours());
+
+
+// console.log(newDate.getMonth());
+
