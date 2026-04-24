fb3f53081044a075b0eb402bdfedacc645dfc39f
shubham jaiswal <shubhamjaiswalit@gmail.com>
2026-04-24 14:13:43 +0530

    new one

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,37 @@
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc = ["superman","flash","batman"]
+
+
+// marvel_heros.push(dc)
+
+// console.log(marvel_heros)
+// console.log(marvel_heros[3][1])
+
+// const all_hero= marvel_heros.concat(dc)
+
+// console.log(all_hero)
+
+
+const all_new_heros = [...marvel_heros,...dc]
+
+// console.log(all_new_heros)
+
+const another_array =[1,2,3,[4,5,6],7,[6,7,[4,5,]]]
+// console.log(another_array)
+
+// const real_another_array = another_array.flat(Infinity)
+ // console.log(real_another_array)
+
+
+// console.log(Array.isArray("shubhu"))
+
+// console.log(Array.from("bro_code"))
+
+// console.log(Array.from({name:"shubhuuu"}))  //intresting
+
+
+let score1= 100
+let score2= 200
+let score3= 300
+
+console.log(Array.of(score1,score2,score3))
