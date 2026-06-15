58fad05ef14a0381c3bee8f083c2948a3ff70390
shubham jaiswal <shubhamjaiswalit@gmail.com>
2026-06-15 13:34:25 +0530

    dom

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,19 @@
+
+
+(function chai(){
+
+    //name iife
+    console.log(`db coonected`);
+})();
+
+//()()// defination _execution
+
+
+
+// semciclon lagana hota doh iifi likhne k liye ; ishe stop hota hai ek
+
+((name)=>{
+
+    //simple iife
+    console.log(`db connected two ${name}`)
+})('shubhu');
